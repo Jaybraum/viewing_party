85667dfd0525759a234c8f14d2e0b4ec878842e8
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-07-06 15:34:39 -0600

    Add error handling for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = user_params
-    user[:email] = user[:email].downcase
-    new_user = User.create(user)
+    if !User.find_by(email: params[:user][:email]).nil?
+      flash[:email_exists_failure] = "This email is already associated with an account. Please log in."
+      redirect_to register_path
+    elsif params[:user][:password] != params[:user][:password_confirmation]
+      flash[:password_confirmation_failure] = "Passwords do not match! Please enter the same password twice."
+      redirect_to register_path
+    else
+      user = user_params
+      user[:email] = user[:email].downcase
+      new_user = User.create(user)
 
-    flash[:success] = "Welcome, #{new_user.email}!"
-    redirect_to root_path
+      flash[:success] = "Welcome, #{new_user.email}!"
+      #DASHBOARD PAGE
+      redirect_to register_path
+    end
   end
 
   private
