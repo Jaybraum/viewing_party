1412e3df4a4e98ccefee53101cc3d53ca01790cc
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-07-06 22:18:50 +0200

    Rubocop edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
@@ -14,6 +13,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:email, :password)
   end
