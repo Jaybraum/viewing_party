40502fcbcf99a9d08244a9b336e1afe11ed09b87
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-07-07 15:14:23 -0600

    Add skeleton of API service and poros object

---
+++ app/poros/movies_data.rb
@@ -0,0 +1,12 @@
+require './app/constants'
+
+class NagerData
+  attr_reader :data
+  def initialize
+    @holidays_data = APIService.connect(Constant::HOLIDAYS_PATH)
+  end
+
+  def next_three_holidays
+    [@holidays_data[0][:name], @holidays_data[1][:name], @holidays_data[2][:name]]
+  end
+end
