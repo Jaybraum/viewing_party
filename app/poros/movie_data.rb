ebf8aaaf3956e95ab45a8091304e76c7d6b706aa
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-07-08 23:44:16 +0200

    Update constant

--- app/poros/movie_data.rb
+++ app/poros/movie_data.rb
@@ -1,6 +1,8 @@
+#require './config/initializers/constants'
+
 class MovieData
   attr_reader :data
   def initialize
-    @movie_data = APIService.connect(Constant::MOVIE_PATH)
+    @movie_data = APIService.connect(MOVIE_PATH)
   end
 end
