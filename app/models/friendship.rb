040ea48c1872bf25065104cd8742ddcd16cf7629
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-07-09 11:45:56 -0600

    Add extra friendship validation

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,6 @@
 class Friendship < ApplicationRecord
-  belongs_to :follower, :class_name => :User,:foreign_key => "follower_id"
-  belongs_to :followed, :class_name => :User,:foreign_key => "followed_id"
+  belongs_to :follower, :class_name => :User, :foreign_key => "follower_id"
+  belongs_to :followed, :class_name => :User, :foreign_key => "followed_id"
 
   def self.users_friends(user)
     friend_ids = where(follower_id: user.id).pluck(:followed_id)
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Friendship, type: :model do
   describe "validations" do
     it {should belong_to(:follower)}
+    it {should belong_to(:followed)}
   end
 
   describe "class methods" do
