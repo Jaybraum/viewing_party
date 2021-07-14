07d80e7197bece684908f8acb275987111f1c770
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-07-14 15:06:46 -0600

    Complete initial build out for dashboard index viewing parties view

--- app/models/party.rb
+++ app/models/party.rb
@@ -19,4 +19,17 @@ class Party < ApplicationRecord
 
     friends
   end
+
+  def get_title
+    MoviesDataFacade.get_movie_title(self.movie_id)
+  end
+
+  def get_host
+    User.find(self.host_id).email
+  end
+
+  def get_participants
+    participant_ids = self.party_participants
+    x = User.where(id: participant_ids).pluck(:email)
+  end
 end
