81693aad852845da4b8da743731e8839bce5c2c2
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-07-14 21:36:27 -0600

    Fix logout path

--- app/facades/movies_data_facade.rb
+++ app/facades/movies_data_facade.rb
@@ -1,31 +1,7 @@
 class MoviesDataFacade
 
-  def self.get_movie_details_object(movie_id)
-    movie = APIService.get_movie_details_json(movie_id)
-    MovieDetailsDataObject.new(movie, self)
-  end
-
-  def self.get_movie_title(movie_id)
-    movie = APIService.get_movie_details_json(movie_id)[:title]
-  end
-
-  def self.get_movie_runtime(movie_id)
-    movie = APIService.get_movie_details_json(movie_id)[:runtime]
-  end
-
   def self.find_movies_by_title(title)
     movies_pages = APIService.movie_search_json(title)
-    all_movies = []
-    movies_pages.each do |movies|
-      movies[1].each do |movie|
-        all_movies << MovieSummaryDataObject.new(movie, self)
-      end
-    end
-    all_movies
-  end
-
-  def self.get_top_movie_objects
-    movies_pages = APIService.get_top_rated_json
     all_movies = []
     movies_pages.each do |movies|
       movies[1].each do |movie|
@@ -42,15 +18,16 @@ class MoviesDataFacade
     end
   end
 
-  def self.limited_cast(movie_id)
-    self.get_cast_objects(movie_id)[0..9]
-  end
-
   def self.get_genre_objects
     genres = APIService.get_genres_json[:genres]
     genres.map do |genre|
       GenreDataObject.new(genre)
     end
+  end
+
+  def self.get_movie_details_object(movie_id)
+    movie = APIService.get_movie_details_json(movie_id)
+    MovieDetailsDataObject.new(movie, self)
   end
 
   def self.get_movie_genres(movie)
@@ -60,11 +37,34 @@ class MoviesDataFacade
     end
   end
 
+  def self.get_movie_runtime(movie_id)
+    movie = APIService.get_movie_details_json(movie_id)[:runtime]
+  end
+
+  def self.get_movie_title(movie_id)
+    movie = APIService.get_movie_details_json(movie_id)[:title]
+  end
+
   def self.get_review_objects(movie_id)
     all_reviews = APIService.get_reviews_json(movie_id)[:results]
     all_reviews.map do |review|
       ReviewDataObject.new(review)
     end
+  end
+
+  def self.get_top_movie_objects
+    movies_pages = APIService.get_top_rated_json
+    all_movies = []
+    movies_pages.each do |movies|
+      movies[1].each do |movie|
+        all_movies << MovieSummaryDataObject.new(movie, self)
+      end
+    end
+    all_movies
+  end
+
+  def self.limited_cast(movie_id)
+    self.get_cast_objects(movie_id)[0..9]
   end
 
   def self.limited_reviews(movie_id)
